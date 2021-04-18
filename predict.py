06ecc84a2e4e67c72f75d4d7636c086fd1670ed0
loosh <loosh888@gmail.com>
2021-04-18 17:08:46 +0300

    last commit

--- predict.py
+++ predict.py
@@ -17,23 +17,23 @@ from json import loads
 #Argument parser 
 def parse_args():
     #parser for user input 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Flowers Classifier")
     parser.add_argument('-i', '--image',
                         required=False,
                         default='./test_images/cautleya_spicata.jpg',
-                        help='./test_images')
+                        help='image path /test_images/XX.jpg')
     parser.add_argument('-m', '--model',
                         required=False,
                         default='./train_model.h5',
-                        help='Train model path')
+                        help='Train model path XX.h5')
     parser.add_argument('-t', '--top_k',
                         required=False,
                         default=5,
-                        help='Top k predections')
+                        help='Top k predections ')
     parser.add_argument('-c', '--category_names',
                         required=False,
                         default='label_map.json',
-                        help='Classes names')
+                        help='Class label names XX.json')
     args = vars(parser.parse_args)
 
     return parser.parse_args()
@@ -95,7 +95,8 @@ def load_model(model_path):
 
 # call the functions 
 def main():
-    
+    #check tensorflow cpu/gpu 
+    print(tf.test.gpu_device_name())
     args = parse_args()  
     classes = load_json(args.category_names)
     predict(args.image ,args.model, args.top_k, classes)
